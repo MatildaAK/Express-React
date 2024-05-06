fea1eae6a406e06323be1a5f0d9f17e4453c7419
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-06 12:02:06 +0200

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,41 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const getBooksFromAPi = async () => {
+export const getBooksFromAPI = async () => {
   const url = "http://localhost:3000/api/books";
   const response = await fetch(url);
   const result = await response.json();
   return result;
-}
-
+};
 
 function App() {
-  const [books, setBooks] : any = useState();
+  const [books, setBooks]: any = useState(null);
 
-  const setBooksFromAPi = async () => {
-    const result = await getBooksFromAPi;
+  const setBooksFromAPI = async () => {
+    const result = await getBooksFromAPI();
+    console.log(result);
     setBooks(result);
   }
 
   useEffect(() => {
-    setBooksFromAPi();
+    setBooksFromAPI();
   }, []);
 
   return (
     <>
       <h2>Book listing</h2>
-      { books ? (
+      {books ? (
         books.books.map((book: any, id: number) => {
           return (
             <article key={id}>
               <h3>{book.title}</h3>
               <p>{book.author}</p>
-              <p>
-                <b>{book.price}</b>
-              </p>
+              <p><b>{book.pages}</b></p>
             </article>
           );
         })
       ) : (
-        <p>There are no books....</p>
+        <p>There are no books...</p>
       )}
     </>
   );
