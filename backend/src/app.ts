0dade844748ae5a8c1e0c9d33b669fd1e071ee93
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-06 12:00:42 +0200

    backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,17 @@
 import express, { Express, Request, Response} from "express";
+import cors from "cors";
 import bookRouter from "./routes/books";
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.use("/books", bookRouter);
+app.use("/api/books", bookRouter);
 
 app.get("/", (req: Request, res: Response) =>{
-res.send("Express + TypeScript Server")
+    res.send("Express + TypeScript Server")
 });
 
 export default app;
