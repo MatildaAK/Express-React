61659997fbf18ca0d49800106e1536f27cafac8f
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-08 11:46:40 +0200

    added :id to routes

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -5,8 +5,8 @@ const bookRouter = Router();
 
 bookRouter.get("/", getAllBooks);
 bookRouter.post("/", addBook);
-bookRouter.get("/", getBook);
-bookRouter.put("/", updateBook);
-bookRouter.delete("/", deleteBook);
+bookRouter.get("/:id", getBook);
+bookRouter.put("/:id", updateBook);
+bookRouter.delete("/:id", deleteBook);
 
 export default bookRouter;
