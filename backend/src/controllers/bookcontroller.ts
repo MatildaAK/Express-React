f1782e9e50981599233438fd3b90bfb94c08ed71
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-08 10:49:08 +0200

    book controller

--- backend/src/controllers/bookcontroller.ts
+++ backend/src/controllers/bookcontroller.ts
@@ -1,4 +1,21 @@
 
+const create = async () => {
+
+}
+const readAll = async () => {
+    
+}
+const read = async () => {
+    
+}
+const update = async () => {
+    
+}
+const deleteOne = async () => {
+    
+}
+
+
 export const getAllBooks = (req: any, res: any) => {
     res.json({totalBooks: 2, books: [
         {
@@ -12,4 +29,17 @@ export const getAllBooks = (req: any, res: any) => {
             pages: 584
         }
     ]});
-};
+}
+
+export const addBook = (req: any, res:any) => {
+
+}
+export const getBook = (req: any, res:any) => {
+    
+}
+export const updateBook = (req: any, res:any) => {
+    
+}
+export const deleteBook = (req: any, res:any) => {
+    
+}
