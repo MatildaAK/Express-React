4426d54a9483d1a52d7581dee15fcb30a938da10
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-09 11:54:52 +0200

    .

--- backend/src/controllers/bookcontroller.ts
+++ backend/src/controllers/bookcontroller.ts
@@ -5,11 +5,11 @@ const create = async (data: IBook) => {
     await Book.create(data);
 };
 const readAll = async () => {
-    const books = await Book.find();
+    const books = await Book.find({});
     return books;
 };
 const read = async (id:any) => {
-   return await Book.findById(id).exec();
+   return await Book.findById(id);
 };
 const update = async (id:any, data: IBook) => {
     return await Book.findByIdAndUpdate(id, data, { new: true });
@@ -18,22 +18,23 @@ const deleteOne = async (id:any) => {
     return await Book.findByIdAndDelete(id); 
 };
 
-export const getAllBooks = (req: any, res: any) => {
-    const books = readAll();
-    res.jstatus(200).json(books);
+export const getAllBooks = async (req: any, res: any) => {
+    const books = await readAll();
+    res.status(200).json(books);
 };
 export const addBook = async (req: any, res:any) => {
     const {title, author, publisher, read} = req.body;
 
     const result = await create({title, author, publisher, read});
 
-    res.status(200).json({message: "Book created successfully:", book: result})
+    res.status(201).json({message: "Book created successfully", book: result})
 };
 export const getBook = async (req: any, res:any) => {
-    const book = await read(req.params.id);
+    // const book = await read(req.params.id);
+    const id = req.params.id;
+    const book = await read(id);
 
     res.status(200).json(book);
-
 };
 export const updateBook = async (req: any, res:any) => {
     const {title, author, publisher, read} = req.body;
@@ -41,12 +42,16 @@ export const updateBook = async (req: any, res:any) => {
 
     const updatedBook = await update(id, {title, author, publisher, read});
 
-    res.status(200).json({message: "Updated succeded", updatedBook: updatedBook});
+    res
+        .status(200)
+        .json({message: "Update succeded", updatedBook: updatedBook});
 };
-export const deleteBook = async  (req: any, res:any) => {
+export const deleteBook = async (req: any, res:any) => {
     const id = req.params.id;
 
     const deletedBook = await deleteOne(id);
 
-    res.status(200).json({message: "Book deleted", deletedBook: deletedBook});
+    res
+        .status(200)
+        .json({message: "Book deleted", deletedBook: deletedBook});
 };
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,15 +1,15 @@
 import { connect } from "mongoose";
 
 const connectDB = async () => {
-   try {
-    const mongoURI = "mongodb://localhost:27017/bookDb";
+  try {
+    const mongoURI: string = "mongodb://localhost:27017/bookDb";
     await connect(mongoURI);
     console.log("MongoDB Connected...");
-    } catch (err:any){
-        console.error(err.message);
-        // Exit process with fail
-        process.exit(1);
-    }
-}
+  } catch (err:any) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
-export default connectDB;
+export default connectDB();
